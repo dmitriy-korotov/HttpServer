4d4a3b059355c414f62525dadd9e01bb0843376b
Dmitriy <dmitriy.korotov.76@mail.ru>
2023-06-05 19:37:25 +0300

    Added tests for Url functions and fixed some bags

--- libs/HttpServerLib/impl/server/http/Url.cpp
+++ libs/HttpServerLib/impl/server/http/Url.cpp
@@ -25,7 +25,8 @@ namespace http::request::url
 
 	bool isPathToSourceFile(const std::string_view& _target)
 	{
-		return (_target.find_last_of('.') != std::string::npos);
+		size_t extention_position = _target.find_last_of('.');
+		return (extention_position != std::string::npos && _target.find_first_of('/', extention_position) == std::string::npos);
 	}
 
 
@@ -40,6 +41,12 @@ namespace http::request::url
 	std::string parseRelativePath(const std::string_view& _target)
 	{
 		size_t question_position = _target.find_first_of('?');
-		return (question_position != std::string::npos) ? std::string(_target.substr(0, question_position)) : std::string(_target);
+		size_t start_server_domen = _target.find_first_of("//");
+		size_t start_relative_path = (start_server_domen == std::string::npos) ? 0 : _target.find_first_of('/', start_server_domen + 2);
+
+		return (question_position != std::string::npos) ?
+								std::string(_target.substr(start_relative_path, question_position - start_relative_path)) 
+								:
+								std::string(_target.substr(start_relative_path, _target.length() - start_relative_path));
 	}
 }
--- tests/src/UrlFunctionsTest.cpp
+++ tests/src/UrlFunctionsTest.cpp
@@ -1,39 +1,72 @@
 #include <boost/test/unit_test.hpp>
-#include <http/QueryStringParser.hpp>
+
+#include <http/Url.hpp>
 
 
 
-BOOST_AUTO_TEST_SUITE(TestQueryStringParser)
+BOOST_AUTO_TEST_SUITE(TestUrlFunctions)
 
-BOOST_AUTO_TEST_CASE(TwoParametrs)
+BOOST_AUTO_TEST_CASE(IsPathToSourceFile)
 {
-	//http::request::url::QSparser _parser("localhost.com?name=Dima&surname=Korotov");
-
-	//auto& parsed_qs = _parser.get();
-
-	//BOOST_TEST(parsed_qs["name"] == "Dima");
-	//BOOST_TEST(parsed_qs["surname"] == "Korotov");
-	BOOST_TEST(2 == 2);
+	bool is_path_to_source = http::request::url::isPathToSourceFile("http://localhost.com/home/index.html");
+	BOOST_TEST(is_path_to_source == true);
 }
 
-BOOST_AUTO_TEST_CASE(ZeroParametrs)
+BOOST_AUTO_TEST_CASE(IsNotPathToSourceFile)
 {
-	/*http::request::url::QSparser _parser("localhost.com?");
-
-	auto& parsed_qs = _parser.get();
-
-	BOOST_TEST(parsed_qs.empty());*/
-	BOOST_TEST(3 == 2);
+	bool is_path_to_source = http::request::url::isPathToSourceFile("http://localhost.com/home/user");
+	BOOST_TEST(is_path_to_source == false);
 }
 
-BOOST_AUTO_TEST_CASE(EmptyString)
+BOOST_AUTO_TEST_CASE(EmptyPath)
 {
-	/*http::request::url::QSparser _parser("localhost.com?");
+	bool is_path_to_source = http::request::url::isPathToSourceFile("");
+	BOOST_TEST(is_path_to_source == false);
+}
 
-	auto& parsed_qs = _parser.get();
+BOOST_AUTO_TEST_CASE(TwoLuevelsDeepth)
+{
+	auto parsed_path = http::request::url::parseRelativePath("http://google.com/home/user");
+	BOOST_TEST(parsed_path == "/home/user");
+}
 
-	BOOST_TEST(parsed_qs.empty());*/
-	BOOST_TEST(3 == 7);
+BOOST_AUTO_TEST_CASE(TwoLuevelsDeepthWidthQueryString)
+{
+	auto parsed_path = http::request::url::parseRelativePath("http://google.com/home/user?name=surname&Dima=Korotov");
+	BOOST_TEST(parsed_path == "/home/user");
+}
+
+BOOST_AUTO_TEST_CASE(ZeroLuevelsDeepth)
+{
+	auto parsed_path = http::request::url::parseRelativePath("http://google.com/");
+	BOOST_TEST(parsed_path == "/");
+}
+
+BOOST_AUTO_TEST_CASE(ZeroLuevelsDeepthWidthQueryString)
+{
+	auto parsed_path = http::request::url::parseRelativePath("http://google.com/?name=surname&Dima=Korotov");
+	BOOST_TEST(parsed_path == "/");
+}
+
+BOOST_AUTO_TEST_CASE(ConvertionEtentions)
+{
+	auto png = http::request::url::convertExtentionToContentType(".png");
+	auto jpg = http::request::url::convertExtentionToContentType(".jpg");
+	auto jpeg = http::request::url::convertExtentionToContentType(".jpeg");
+	auto ico = http::request::url::convertExtentionToContentType(".ico");
+	auto pdf = http::request::url::convertExtentionToContentType(".pdf");
+	auto html = http::request::url::convertExtentionToContentType(".html");
+	auto css = http::request::url::convertExtentionToContentType(".css");
+	auto js = http::request::url::convertExtentionToContentType(".js");
+
+	BOOST_TEST(png == "image/png");
+	BOOST_TEST(jpg == "image/jpg");
+	BOOST_TEST(jpeg == "image/jpeg");
+	BOOST_TEST(ico == "image/ico");
+	BOOST_TEST(pdf == "application/pdf");
+	BOOST_TEST(html == "text/html");
+	BOOST_TEST(css == "text/css");
+	BOOST_TEST(js == "text/javascript");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
